60fda7c30554fb75656dffde5b1478f6cfb035fd
Tear♪ <marach5@gmail.com>
2017-12-13 00:04:04 +0000

    Make things work, hopefully

---
+++ alphes-alt/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "alphes-alt",
+	"servers": [
+		"https://raw.githubusercontent.com/Tearff/Tear/master/alphes-alt/"
+	],
+	"title": "Alphes-style portraits (art by kaoru)"
+}
